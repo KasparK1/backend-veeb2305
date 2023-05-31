77b38094dba54934e70e75766e912003fa5fb706
KasparK1 <130385209+KasparK1@users.noreply.github.com>
2023-05-31 22:14:38 +0300

    kommit 1

--- index.js
+++ index.js
@@ -1,17 +1,30 @@
 const express = require('express');
+const pool = require('./config');
+ 
 const app = express();
-
+app.use(express.json());
+ 
 app.get('/', (req, res) => {
     res.send('Simple API homepage');
-})
-
-app.get('/api/items', (req, res) => {
-    res.send('Sending a list of items from the DB ...')
-})
-app.post('/api/items', (req, res) => {
-    res.status(201).send('Sent the new data to the DB ...')
-})
-
+});
+ 
+app.get('/api/treks', async (req, res) => {
+  try {
+    const treks = await pool.query(
+        'SELECT * FROM treks;'
+    );
+    res.json({ treks });
+  } catch (error) {
+    console.log(error);
+    res.status(500).send(error.message)
+  }
+});
+ 
+app.post('/api/treks', (req, res) => {
+  res.status(201).send('Sent the new data to the DB ...')
+});
+ 
 app.listen(5070, () => {
     console.log("Server running on port 5070");
-})
+});
+ 
